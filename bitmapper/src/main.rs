aa2c96397d2a212e8995f1df80c78fc291ab7fdf
Nimrod Libman <>
2020-10-26 15:17:04 +0000

    Reading from file

--- bitmapper/src/main.rs
+++ bitmapper/src/main.rs
@@ -1,3 +1,36 @@
+use image;
+
+use std::env;
+use std::path::Path;
+
+use image::{GenericImageView, Pixel};
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    //we need one argument; the input image
+    let input_filename = args[1].clone();
+
+    let img = image::open(&Path::new(&input_filename)).unwrap();
+
+    let (width, height) = img.dimensions();
+    println!("{}", width);
+    println!("{}", height);
+
+    for x in 0..width - 1 {
+        for y in 0..height {
+            let pixel = img.get_pixel(x, y);
+            //decode the pixel to a hex
+            if y != 0 {
+                print!(",");
+            }
+            print!("{:?}", pixel.to_rgb());
+        }
+        println!(";");
+    }
 }
+
+
+
+
