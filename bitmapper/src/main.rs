c7264bcdfd169c5cf23727ed2ed56642025b09e4
Nimrod Libman <>
2020-10-29 10:30:49 +0000

    Now writes to file rather than to stdout, also  cleaner

--- bitmapper/src/main.rs
+++ bitmapper/src/main.rs
@@ -1,36 +1,49 @@
 use image;
-
+use std::fs::File;
 use std::env;
 use std::path::Path;
 
 use image::{GenericImageView, Pixel};
+use std::io::{Write, Error};
 
+
+fn write(out: &mut File, to_write: &str) {
+    out.write_all(to_write.as_bytes()).expect("Couldn't write to file");
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    //we need one argument; the input image
+    if args.len() != 3 {
+        print!("Needs exactly two arguments; <input_file> <output_file>");
+    }
     let input_filename = args[1].clone();
+    let output_filename = args[2].clone();
 
-    let img = image::open(&Path::new(&input_filename)).unwrap();
+    let img = image::open(&input_filename).expect("couldn't open image");
 
     let (width, height) = img.dimensions();
-    println!("{};", width);
-    println!("{};", height);
+
+
+    let mut out = File::create(&output_filename).expect("couldn't create file:");
+
+    let header = format!("{};{};\n", width, height);
+    write(&mut out, &header);
+
+
     for y in 0..height {
         for x in 0..width - 1 {
             let pixel = img.get_pixel(x, y);
             //decode the pixel to a hex
             if x != 0 {
-                print!(",");
+                write(&mut out, ",");
             }
-            //print!("{:?}", pixel.to_rgb());
             let rgb = pixel.to_rgb();
-            print!("{:02x}", rgb[0]);
-            print!("{:02x}", rgb[1]);
-            print!("{:02x}", rgb[2]);
+            let hex = format!("{:02x}{:02x}{:02x}", rgb[0], rgb[1], rgb[1]);
+            write(&mut out, &hex);
         }
-        println!(";");
+
+        write(&mut out, ";\n");
     }
 }
 
